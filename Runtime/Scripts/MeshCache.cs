a7d2ecda9ece681c750033f76e29bed56bb7c298
VladV <vanifatov.vlad@yandex.ru>
2022-11-04 15:38:58 +0400

    Add domain reload support

--- Runtime/Scripts/MeshCache.cs
+++ Runtime/Scripts/MeshCache.cs
@@ -1,19 +1,42 @@
-namespace CodeWriter.MeshAnimation {
+namespace CodeWriter.MeshAnimation
+{
     using System.Collections.Generic;
     using UnityEngine;
+#if UNITY_EDITOR
+    using UnityEditor;
 
-    public static class MeshCache {
+#endif
+
+    public static class MeshCache
+    {
         private static readonly HashSet<Mesh> CachedUv = new HashSet<Mesh>();
 
-        public static void GenerateSecondaryUv(Mesh mesh) {
-            if (CachedUv.Contains(mesh)) {
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            EditorApplication.playModeStateChanged += change =>
+            {
+                if (change == PlayModeStateChange.EnteredEditMode)
+                {
+                    CachedUv.Clear();
+                }
+            };
+        }
+#endif
+
+        public static void GenerateSecondaryUv(Mesh mesh)
+        {
+            if (CachedUv.Contains(mesh))
+            {
                 return;
             }
 
             CachedUv.Add(mesh);
 
             var uvs = new Vector2[mesh.vertexCount];
-            for (int i = 0; i < uvs.Length; i++) {
+            for (int i = 0; i < uvs.Length; i++)
+            {
                 uvs[i] = new Vector2(1f * i, 0);
             }
 
